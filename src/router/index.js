c2f3ac29071d49180889e697837969b4f440264b
Greg <chrisnamahendra@gmail.com>
2019-11-18 08:09:00 +0700

    tugas paw

---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue' 
+import Router from 'vue-router' 
+ 
+const DashboardLayout = () => import(/* webpackChunkName: "dashboard" */ '../components/dashboardLayout.vue') 
+const loginLayout = () => import(/* webpackChunkName: "dashboard" */ '../components/loginLayout.vue') 
+
+function loadView(view) {
+    return () => import(/* webpackChunkName: "view[request]" */ `../components/dashboardContents/${view}.vue`) } 
+ 
+    const routes = [     
+        {       
+            path: '/dashboard',       
+            component: DashboardLayout,       
+            children: [         
+                {           
+                    name: 'Dashboard',           
+                    path: '/dashboard',           
+                    component: loadView('dashboard')         
+                },
+                {           
+                    name: 'UserController',           
+                    path: '/userController',           
+                    component: loadView('userController')         
+                },
+                {           
+                    name: 'BranchController',           
+                    path: '/branchController',           
+                    component: loadView('branchController')         
+                }        
+            ]     
+        },
+        {
+            path: '/',
+            component: loginLayout,
+            name: 'loginLayout'
+        }   
+    ]   
+    Vue.use(Router) 
+     
+    const router = new Router({mode: 'history', routes: routes}) 
+     
+    export default router   
